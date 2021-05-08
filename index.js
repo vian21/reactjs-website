5d59a6f217d5dad2ba4fca1a694e772fc7f8c49d
patrick31 <patrick31@Macbook-Pro.local>
2021-05-08 17:47:37 -0400

    config 9.0

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile('index.html');
     });
 } else {
     app.use((req, res) => {
